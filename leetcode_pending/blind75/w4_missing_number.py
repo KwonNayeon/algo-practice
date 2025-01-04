8d645b0d528c82d9b8bb7b4a3f5bfd42cb0b7637
Nayeon <nayeonkn0330@gmail.com>
2025-01-04 19:20:06 +0900

    Update w4_missing_number.py

--- leetcode_pending/blind75/w4_missing_number.py
+++ leetcode_pending/blind75/w4_missing_number.py
@@ -1,12 +1,11 @@
 """
 Constraints:
-1. 
-2. 
+   1. 
+   2. 
 
 Time Complexity: -
-
 Space Complexity: -
 
 Future Improvements:
-- 정렬을 사용하지 않고 O(n)에 푸는 풀이 방법 고민해보기
+  - 정렬을 사용하지 않고 O(n)에 푸는 풀이 방법 고민해보기
 """
