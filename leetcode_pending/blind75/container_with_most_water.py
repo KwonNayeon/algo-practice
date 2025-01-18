08790be91377dafe3ad6692adb924444bb712b1b
Nayeon <nayeonkn0330@gmail.com>
2025-01-18 21:01:02 +0900

    Create container_with_most_water.py

---
+++ leetcode_pending/blind75/container_with_most_water.py
@@ -0,0 +1,18 @@
+"""
+Constraints:
+- n == height.length
+- 2 <= n <= 10^5
+- 0 <= height[i] <= 10^4
+
+Time Complexity: -
+Space Complexity: -
+
+Problem Summary:
+1. Area Formula:
+  area = distance between pillars × min(height1, height2)
+
+2. Key Points for Maximum Area:
+  - Longer distance between pillars is better
+  - Higher pillars are better
+  - The shorter pillar determines the water height
+"""
