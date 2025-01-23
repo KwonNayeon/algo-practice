a74a93805e21d1b05e5c0ac0fbcc01d76b56adfe
Nayeon <nayeonkn0330@gmail.com>
2025-01-23 15:08:32 +0900

    Update w6_container_with_most_water.py

--- leetcode_pending/blind75/w6_container_with_most_water.py
+++ leetcode_pending/blind75/w6_container_with_most_water.py
@@ -4,15 +4,27 @@ Constraints:
 - 2 <= n <= 10^5
 - 0 <= height[i] <= 10^4
 
-Time Complexity: -
-Space Complexity: -
+Time Complexity: O(n)
+Space Complexity: O(1)
 
 Problem Summary:
 1. Area Formula:
-  area = distance between pillars × min(height1, height2)
-
+ area = distance between pillars × min(height1, height2)
 2. Key Points for Maximum Area:
-  - Longer distance between pillars is better
-  - Higher pillars are better
-  - The shorter pillar determines the water height
+ - Longer distance between pillars is better
+ - Higher pillars are better
+ - The shorter pillar determines the water height
 """
+
+class Solution:
+   def maxArea(self, height: List[int]) -> int:
+       max_area = 0
+       s, e = 0, len(height) - 1
+       while s < e:
+           area = (e - s) * min(height[s], height[e])
+           max_area = max(area, max_area)
+           if height[s] < height[e]:
+               s += 1
+           else:
+               e -= 1
+       return max_area
